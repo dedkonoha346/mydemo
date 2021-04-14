cdb65b8469bfeb976b023b367991737d7619ff71
dedkonoha346 <w1ns-jh@yandex.ru>
2021-04-14 21:58:14 +0300

    first blood

---
+++ print (hello world).py
@@ -0,0 +1,6 @@
+var_orange = input("Сколько апельсинов: ")
+price_orange = input("Цена за апельсин: ")
+var_orange = int(var_orange)
+price_orange = float(price_orange)
+summa = var_orange * price_orange
+print("У тебя {0} апельсинок, с тебя {1} рублей".format(var_orange, summa))
