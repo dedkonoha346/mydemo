6345efd4815bb99443c83ca0770fae9f43acc177
dedkonoha346 <w1ns-jh@yandex.ru>
2021-04-14 22:07:19 +0300

    xyinya

--- print (hello world).py
+++ print (hello world).py
@@ -4,3 +4,4 @@ var_orange = int(var_orange)
 price_orange = float(price_orange)
 summa = var_orange * price_orange
 print("У тебя {0} апельсинок, с тебя {1} рублей".format(var_orange, summa))
+я сосал меня ебали
